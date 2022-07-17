96ee8909cf2fc860d52c3d0e45ff2c7d1393e34b
Prantik <prantiknoor@gmail.com>
2022-07-17 22:41:48 +0600

    used fetch() instead of XMLHttpRequest()
    also used js Promise

--- advice-generator-app/script.js
+++ advice-generator-app/script.js
@@ -1,27 +1,27 @@
 const adviceId = document.querySelector("#advice-id");
-const quoteEl = document.querySelector(".card__quote")
-const diceButton = document.querySelector('.card__dice-button');
+const quoteEl = document.querySelector(".card__quote");
+const diceButton = document.querySelector(".card__dice-button");
 
 const apiEndpoint = "https://api.adviceslip.com/advice";
 
 function fetchAdvice() {
-    const http = new XMLHttpRequest();
-    http.open('GET', apiEndpoint, false);
-    http.send(null);
-    return JSON.parse(http.responseText).slip;
+  return new Promise((resolve, reject) => {
+    fetch(apiEndpoint, { cache: "no-cache" })
+      .then((response) => response.json())
+      .then((data) => resolve(data.slip));
+  });
 }
 
-function newAdvice() {
-    diceButton.classList.add("card__dice-button--rotate");
-    
-    const advice = fetchAdvice();
+async function newAdvice() {
+  diceButton.classList.add("card__dice-button--rotate");
 
-    adviceId.innerText = advice.id;
-    quoteEl.innerText = advice.advice;
-    diceButton.classList.remove("card__dice-button--rotate");
+  const advice = await fetchAdvice();
 
+  adviceId.innerText = advice.id;
+  quoteEl.innerText = advice.advice;
+  diceButton.classList.remove("card__dice-button--rotate");
 }
 
-diceButton.addEventListener('click', (e) => newAdvice());
+diceButton.addEventListener("click", (e) => newAdvice());
 
 window.onload = newAdvice();
