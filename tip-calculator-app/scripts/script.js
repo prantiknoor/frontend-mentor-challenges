97a1e34a5d608d6be014135ed334837d6c9c11d8
Prantik <prantiknoor@gmail.com>
2022-08-01 22:23:20 +0600

    code more function & format file

--- tip-calculator-app/scripts/script.js
+++ tip-calculator-app/scripts/script.js
@@ -1,51 +1,10 @@
 // inputs
 const billInput = document.querySelector("#input-bill");
 const peopleInput = document.querySelector("#input-people");
-const tipCustomInput = document.querySelector("#custom-percent");
-const tipCustomLabel = document.querySelector(".custom");
 
 const tipAmountEl = document.querySelector("#tip-amount");
 const totalAmountEl = document.querySelector("#total-amount");
 
-const resetButton = document.querySelector('button[type="reset"]');
-
-tipCustomLabel.addEventListener("focus", (e) => {
-  console.log("TODO: Select all texts");
-});
-
-tipCustomLabel.addEventListener("input", (e) => {
-  let value = 0;
-  if (validateCustomTip()) {
-    value = tipCustomLabel.innerText;
-  }
-  tipCustomInput.value = value;
-});
-
-function validateCustomTip() {
-  const innerText = tipCustomLabel.innerText;
-
-  if (innerText.length > 4) {
-    tipCustomLabel.innerText = innerText.slice(0, 5);
-  }
-  if (/\n/g.test(innerText)) {
-    tipCustomLabel.innerText = innerText.replaceAll("\n", "");
-  }
-  if (!(innerText && /^\d*\.?\d*$/g.test(innerText))) {
-    tipCustomLabel.classList.add("custom--invalid");
-    return false;
-  }
-  if (!(innerText > 0 && innerText <= 100)) {
-    tipCustomLabel.classList.add("custom--invalid");
-    return false;
-  }
-
-  tipCustomLabel.classList.remove("custom--invalid");
-  return true;
-}
-
-[billInput, peopleInput].map((input) => {
-  input.addEventListener("input", (e) => calculateTip());
-});
 
 function calculateTip() {
   const selectedTipPercent = document.querySelector(
@@ -61,23 +20,42 @@ function calculateTip() {
   if (empty) return;
 
   const total = billInput.value / peopleInput.value;
-  const tip = selectedTipPercent.value / 100 * total;
+  const tip = (selectedTipPercent.value / 100) * total;
 
   tipAmountEl.innerText = "$" + tip.toFixed(2);
   totalAmountEl.innerText = "$" + total.toFixed(2);
 }
 
-
-function reset() {
+function resetAll() {
   tipAmountEl.innerText = "$0.00";
   totalAmountEl.innerText = "$0.00";
 }
 
-resetButton.addEventListener("click", (e) => reset());
+function updateValue(event) {
+  event.target.control.value = event.target.innerText;
+}
 
-const labels = document.querySelectorAll(".calculator__input-tip label:not(:first-child)");
-labels.forEach((label) => {
-  label.addEventListener("click", (e) => calculateTip());
-});
+function toggleText(event) {
+  const text = event.target.innerText;
+  if (!/^\d*\.?\d*$/g.test(text)) {
+    event.target.innerText = "";
+  }
+  if (event.type == "focusout" && !text) {
+    event.target.innerText = "Custom";
+  }
+}
 
-tipCustomLabel.addEventListener("input", (e) => calculateTip());
+function inputOnlyNumber(event) {
+  const text = event.target.innerText + event.key;
+  if (/^\d*\.?\d*$/g.test(text) && text.length <= 5) {
+    return text < 100;
+  } else {
+    return false;
+  }
+}
+
+function selectRadio(event) {
+  if (event.code == "Enter" || event.code == "Space") {
+    event.target.control.checked = true;
+  }
+}
